17f814c7289c335e3bb74ecbb5d561f564ee7ddf
yjin0034 <yjin0034@naver.com>
2023-01-12 16:36:15 +0900

    EP15 유저 Follow 구현 - react 화면 구현

--- frontend/src/components/Suggestion.js
+++ frontend/src/components/Suggestion.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { Avatar, Button } from "antd";
-import { UserOutlined } from '@ant-design/icons';
 import "./Suggestion.scss";
 
-export default function Suggestion({ suggestionUser }) {
-  const { username, name, avatar_url } = suggestionUser;
+export default function Suggestion({ suggestionUser, onFollowUser }) {
+  const { username, name, avatar_url, is_follow } = suggestionUser;
   return (
     <div className="suggestion">
       <div className="avatar">
@@ -22,7 +21,12 @@ export default function Suggestion({ suggestionUser }) {
         {name.length === 0 ? username : name}
       </div>
       <div className="action">
-        <Button size="small">Follow</Button>
+        {is_follow && "팔로잉 중"}
+        {!is_follow && (
+          <Button size="small" onClick={() => onFollowUser(username)}>
+            Follow
+          </Button>
+          )}
       </div>
     </div>
   );
