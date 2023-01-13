629d3cb5c3e4fb4832fd5f78bf938af3771bbcd2
yjin0034 <yjin0034@naver.com>
2023-01-13 22:00:38 +0900

    EP17 포스팅 목록 API 및 화면 정리

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -4,16 +4,23 @@ import { HeartOutlined, HeartFilled, UserOutlined } from "@ant-design/icons";
 import "./Post.scss";
 
 function Post({ post }) {
-  const { caption, location, photo } = post;
+  const { author, caption, location, photo, tag_set, like_user_set } = post;
+  const { username, name, avatar_url } = author;
+
   return (
     <div className="post">
       <Card 
         hoverable 
         cover={<img src={photo} alt={caption} />}
-        actions={[<HeartFilled />]}
+        actions={[<HeartOutlined />]}
       >
         <Card.Meta 
-          avatar={<Avatar size="large" icon={<UserOutlined />} />} 
+          avatar={
+            <Avatar 
+              size="large" 
+              icon={<img src={`http://localhost:8000` + avatar_url} alt={username} />} 
+            />
+          } 
           title={location} 
           description={caption} 
         />
